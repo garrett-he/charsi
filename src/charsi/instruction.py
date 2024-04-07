bdc04d12296657438f187de0877812560aa57946
Garrett HE <garrett.he@outlook.com>
2024-04-07 10:16:34 +0800

    Add class to invoke instructions

--- src/charsi/instruction.py
+++ src/charsi/instruction.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import re
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict, Callable
 
 from .utils import split_text
 
@@ -31,9 +31,47 @@ class Instruction:
     args: List[str]
 
 
+class InstructionInvoker:
+    _handlers: Dict[str, Callable]
+
+    default: InstructionInvoker
+
+    def __init__(self):
+        self._handlers = {}
+
+    def register(self, name: str, handler: Callable):
+        if name in self._handlers:
+            raise InstructionConflictError(name)
+
+        self._handlers[name] = handler
+
+    def unregister(self, name: str):
+        if name not in self._handlers:
+            raise InstructionUndefinedError(name)
+
+        del self._handlers[name]
+
+    def is_registered(self, name: str):
+        return name in self._handlers
+
+    def invoke(self, inst: Instruction, text: str) -> str:
+        if not self.is_registered(inst.name):
+            raise InstructionUndefinedError(inst.name)
+
+        return self._handlers[inst.name](text, *inst.args)
+
+
 class _InstructionError(Exception):
     ...
 
 
 class InstructionFormatError(_InstructionError):
+    ...
+
+
+class InstructionConflictError(_InstructionError):
+    ...
+
+
+class InstructionUndefinedError(_InstructionError):
     ...
--- tests/test_instruction.py
+++ tests/test_instruction.py
@@ -1,5 +1,5 @@
 import pytest
-from charsi.instruction import parse, InstructionFormatError
+from charsi.instruction import parse, InstructionFormatError, InstructionInvoker, InstructionConflictError, InstructionUndefinedError
 
 
 def test_instruction_parse():
@@ -18,3 +18,32 @@ def test_instruction_parse():
         parse('Invalid Instruction:')
 
     assert str(e.value) == 'Invalid Instruction:'
+
+
+def test_instruction_invoker():
+    def handler(text: str, *args):
+        return f'{text}:{"_".join(args)}'
+
+    invoker = InstructionInvoker()
+    inst = parse('TestInstruction[query]: arg1, arg2')
+
+    invoker.register('TestInstruction', handler)
+    assert invoker.is_registered('TestInstruction')
+
+    with pytest.raises(InstructionConflictError) as e:
+        invoker.register('TestInstruction', handler)
+    assert str(e.value) == 'TestInstruction'
+
+    invoker.unregister('TestInstruction')
+    assert not invoker.is_registered('TestInstruction')
+
+    with pytest.raises(InstructionUndefinedError) as e:
+        invoker.invoke(inst, '')
+    assert str(e.value) == 'TestInstruction'
+
+    with pytest.raises(InstructionUndefinedError) as e:
+        invoker.unregister('TestInstruction')
+    assert str(e.value) == 'TestInstruction'
+
+    invoker.register('TestInstruction', handler)
+    assert invoker.invoke(inst, 'TestString') == 'TestString:arg1_arg2'
