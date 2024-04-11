7c590e10e584f1806071edc7f86bc305d9beac00
Garrett HE <garrett.he@outlook.com>
2024-04-11 10:20:43 +0800

    Create command group

--- src/charsi/__main__.py
+++ src/charsi/__main__.py
@@ -1,6 +1,7 @@
 import click
 
 from charsi import __version__
+from charsi.commands import command_group
 
 
 def print_version(ctx: click.Context, _, value: bool):
@@ -11,7 +12,7 @@ def print_version(ctx: click.Context, _, value: bool):
     ctx.exit()
 
 
-@click.command()
+@click.group(commands=command_group)
 @click.option('--version', help='Show version information.', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
 def cli():
     """A command-line tool to help game modders build string resources for Diablo II: Resurrected."""
