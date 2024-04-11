f7e4548aefafa3806c61b6233ffe5642ed493b29
Garrett HE <garrett.he@outlook.com>
2024-04-11 11:33:09 +0800

    Add dependencies for pyinstaller

---
+++ src/charsi/commands/make.py
@@ -0,0 +1,23 @@
+import sys
+from typing import IO
+
+import click
+
+from charsi.strings import StringTable
+from charsi.recipe import Recipe
+
+
+@click.command('make')
+@click.option('--table-file', help='Path of string table file.', metavar='FILE', type=click.File(mode='r', encoding='utf-8-sig'), required=True)
+@click.argument('recipe-file', metavar='FILE', type=click.File(mode='r', encoding='utf-8-sig'), required=True)
+def make_command(table_file: IO, recipe_file: IO):
+    """Build a string table with the specified recipe."""
+
+    recipe = Recipe()
+    recipe.read(recipe_file)
+
+    tbl = StringTable()
+    tbl.read(table_file)
+
+    recipe.build(tbl)
+    tbl.dump(sys.stdout)
